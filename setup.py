c1ed537ef578de9213f85c292812878bb76b94c6
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-03-26 21:39:44 -0400

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='weather',
+    version='0.1',
+    description='Weather in console',
+    author='Maksim Rakitin',
+    url='https://github.com/mrakitin/web',
+    packages=['weather'],
+)
